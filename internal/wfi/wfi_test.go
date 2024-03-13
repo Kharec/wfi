20b4c3b22ac0a78aceae3085a1464ba899bcc0c7
Sandro CAZZANIGA <sandro@cazzaniga.fr>
2024-03-13 09:34:04 +0100

    feature: rewrite tests using Run()

--- internal/wfi/wfi_test.go
+++ internal/wfi/wfi_test.go
@@ -4,20 +4,27 @@ import (
 	"testing"
 )
 
-func TestTCPPort(t *testing.T) {
-	if !TryPort("tcp", "google.com", "443") {
-		t.Fatal()
+func TestTryPort(t *testing.T) {
+	testCases := []struct {
+		name  string
+		proto string
+		host  string
+		port  string
+		want  bool
+	}{
+		{"ValidTCPPort", "tcp", "google.com", "443", true},
+		{"LocalTCPPort", "tcp", "localhost", "3306", true},
+		{"ValidUDPPort", "udp", "1.1.1.1", "53", true},
+		{"InvalidHostname", "tcp", "souzej.pk", "239", false},
+		{"InvalidTCPPort", "tcp", "facebook.com", "92821", false},
+		{"InvalidUDPPort", "udp", "github.com", "99999", false},
 	}
-}
-
-func TestLocalhostTCP(t *testing.T) {
-	if !TryPort("tcp", "localhost", "3306") {
-		t.Fatal("Maybe run `make test-dependency`")
-	}
-}
-
-func TestUDPPort(t *testing.T) {
-	if !TryPort("udp", "1.1.1.1", "53") {
-		t.Fatal()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			got := TryPort(tc.proto, tc.host, tc.port)
+			if got != tc.want {
+				t.Errorf("TryPort(%s, %s, %s) = %v; want %v", tc.proto, tc.host, tc.port, got, tc.want)
+			}
+		})
 	}
 }
